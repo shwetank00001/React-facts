e6dd9a8df02aa41fd37cbeecb0e2dd1ff87478e2
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-02-06 22:02:52 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <h1>
+            Fun facts about Me
+        </h1>
+        <ul>
+          <li>I bench 225</li>
+          <li>i go jim</li>
+          <li>My birthday on 14th March</li>
+          <li>1st project using React</li>
+          <li>Will get rich</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../react-logo.png'
+
+const Nav = () => {
+  return (
+    <div>
+        
+        <header>
+          <nav>
+            <img src={logo} alt="react-logo" width="150px"/>
+            <h2>ReactFacts</h2>
+            <h3>React Course- Project1</h3>
+          </nav>
+        </header>
+
+    </div>
+  )
+}
+
+export default Nav
